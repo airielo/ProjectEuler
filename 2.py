3727999fe089b7ccf292cc34d5bd2346895c9d13
airielo <37617801+airielo@users.noreply.github.com>
2019-06-29 13:10:48 +0200

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,7 +1,20 @@
+#generate Fibonacci list
 fibRange=[1,2]
 while fibRange[-1]<4000000:
     lastEl=sum(fibRange[-2:])
     fibRange.append(lastEl)
 print(fibRange[:-1])
+
+#conditional sum
 evens=sum(i for i in fibRange[:-1] if i%2==0)
 print(evens)
+
+# 2nd approach for Fibonacci list
+
+res = [1,2]
+a=1
+b=2
+while b <100:
+    a, b = b, a+b
+    res.append(b)
+res
