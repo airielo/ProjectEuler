766ba314f87604b347994d56da4d833289a46eb9
airielo <37617801+airielo@users.noreply.github.com>
2019-06-29 13:06:53 +0200

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,6 +1,7 @@
 fibRange=[1,2]
-while fibRange[-1]<100:
+while fibRange[-1]<4000000:
     lastEl=sum(fibRange[-2:])
     fibRange.append(lastEl)
 print(fibRange[:-1])
-
+evens=sum(i for i in fibRange[:-1] if i%2==0)
+print(evens)
